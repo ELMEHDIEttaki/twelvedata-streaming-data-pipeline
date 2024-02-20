8abe21f38521032ab79c918658b2eb49c3371ce4
ELMEHDIEttaki <el.ettaki22@gmail.com>
2024-02-20 01:18:24 +0100

    second commit

--- Ingestion/requests_test.py
+++ Ingestion/requests_test.py
@@ -4,7 +4,7 @@ import warnings
 # Suppress FutureWarnings
 #warnings.simplefilter(action='ignore', category=FutureWarning)
 # Initialize client - apikey parameter is requiered
-td = TDClient(apikey="3a08aff23f824e18b12b51581ed3e109")
+td = TDClient(apikey="")
 
 # Construct the necessary time series
 ts = td.time_series(
